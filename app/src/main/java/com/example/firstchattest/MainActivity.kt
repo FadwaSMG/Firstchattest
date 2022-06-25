bbb460736e6e5f559a9aa26b1f29029dcc301e7d
fadwa <fadwa.semghouni@uir.ac.ma>
2022-06-25 17:49:33 +0300

    whatever

--- app/src/main/java/com/example/firstchattest/MainActivity.kt
+++ app/src/main/java/com/example/firstchattest/MainActivity.kt
@@ -18,6 +18,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var mAuth: FirebaseAuth
     private lateinit var mDbRef: DatabaseReference
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
